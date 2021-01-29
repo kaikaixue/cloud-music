07f23896383cd84dfcb57029fdc03067f672ea4a
xuekaikai <2780739366@qq.com>
2021-01-29 21:15:26 +0800

    音乐播放页布局

--- cloudfunctions/music/index.js
+++ cloudfunctions/music/index.js
@@ -28,10 +28,17 @@ app.router('playlist', async (ctx, next) => {
   })
 })
 
+//根据歌单id获取歌单详情
 app.router('musiclist', async (ctx, next) => {
   console.log('######' + event.playlistId)
   const res = await axios.get(`${BASE_URL}/playlist/detail?id=${parseInt(event.playlistId)}`)
   console.log('######' + res)
+  ctx.body = res.data
+})
+
+//根据歌曲id获取歌曲播放的url
+app.router('musicUrl', async(ctx, next) => {
+  const res = await axios.get(`${BASE_URL}/song/url?id=${event.musicId}`)
   ctx.body = res.data
 })
 
